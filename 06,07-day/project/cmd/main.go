0ac8ab8d19909c8fdc567a70bd22b6931294552d
Jainam Panchal <jainampanchal1@gmail.com>
2026-05-12 20:12:28 +0530

    chore

--- 06,07-day/project/cmd/main.go
+++ 06,07-day/project/cmd/main.go
@@ -33,7 +33,7 @@ func main() {
 
 	newDriver := &models.Driver{
 		ID:       models.NewDriverID(),
-		Name:     "Jainam",
+		Name:     "Amit",
 		Location: models.Location{Lat: 19.0760, Lng: 72.8777},
 		Status:   models.DriverOffline,
 		Rating:   4.8,
